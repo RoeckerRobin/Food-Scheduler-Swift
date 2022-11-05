793f425514ae65f81b2c6cc1fa1e1105f5ff8e91
Robin Röcker <robin.roecker@studmail.htw-aalen.de>
2022-11-06 00:33:34 +0100

    Inital Commit

---
+++ Food Scheduler Swift/Food_Scheduler_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_Scheduler_SwiftApp.swift
+//  Food Scheduler Swift
+//
+//  Created by Robin Röcker on 06.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Food_Scheduler_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
